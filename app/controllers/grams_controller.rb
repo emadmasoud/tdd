97dfe9c1b483e07bacc0652ec45066563c9f500d
Emad Masoud <emad3000@gmail.com>
2019-09-07 16:06:08 +0000

    recent update

---
+++ app/controllers/grams_controller.rb
@@ -0,0 +1,9 @@
+class GramsController < ApplicationController
+  def new
+    @gram = Gram.new  
+    
+  end
+  
+  def index
+  end
+end
---
+++ spec/controllers/grams_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe GramsController, type: :controller do
+  describe "grams#index action" do
+    it "should successfully show the page" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+
+  describe "grams#new action" do
+    it "should successfully show the new form"  do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+    
+
+  end
+end
